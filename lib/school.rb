8b5e98222a341dcb1c162eabde2fa1d0f90e53f3
James Min <jmin499@gmail.com>
2019-10-29 20:39:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
 
   def add_student(student, grade)
     if(!@roster[grade])
-      @roster[grade] = []
+      @roster[grade] = [student]
     else
       @roster[grade] << student
     end
