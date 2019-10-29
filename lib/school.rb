5c74a2e9b3edd386b84eb5488dd396dc0c382adb
James Min <jmin499@gmail.com>
2019-10-29 20:14:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,25 @@
 # code here!
 class School
   
+  attr_reader :name, :student, :grade
+
+  STUDENTS = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_student(student, grade)
+    STUDENTS << {grade => student}
+  end
+  
+  def roster
+    hash = {}
+    STUDENTS.each do |item|
+      grade = item.keys[0]
+      student = item[grade]
+      hash[grade] = student
+    end
+    hash
+  end
 end
